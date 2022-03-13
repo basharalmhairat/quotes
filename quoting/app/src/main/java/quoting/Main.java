af917fa0a6aa31e5748d8ca73ea38a885d14da25
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-03-13 22:45:29 +0300

    solved

--- quoting/app/src/main/java/quoting/Main.java
+++ quoting/app/src/main/java/quoting/Main.java
@@ -2,6 +2,9 @@ package quoting;
 import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class Main {
@@ -18,6 +21,25 @@ public class Main {
     }
     public static void main(String[] args) throws Exception {
 
+        URL quoteURL = new URL("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
+
+        HttpURLConnection quoteConnection = (HttpURLConnection) quoteURL.openConnection();
+
+        quoteConnection.setRequestMethod("GET");
+
+        InputStreamReader quoteInputStreamReader = new InputStreamReader(quoteConnection.getInputStream());
+        BufferedReader quoteBufferedReader = new BufferedReader(quoteInputStreamReader);
+        String quoteOnline = quoteBufferedReader.readLine();
+        System.out.println(quoteOnline);
+
+        Gson gson = new Gson();
+        Quote quoteFromURL = gson.fromJson(quoteOnline, Quote.class);
+        System.out.println(quoteFromURL);
+
+        File quotingOnline = new File("./quotingOnline.json");
+        try (FileWriter dittoFileWriter = new FileWriter(quotingOnline)) {
+            gson.toJson(quoteFromURL, dittoFileWriter);
+        }
         System.out.println(quotesView("./src/main/resources/quotes.json"));
 
     }
